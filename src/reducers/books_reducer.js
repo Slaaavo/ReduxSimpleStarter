22883affbcf913bd79a7f61a17d44af7acf83bb1
Jaroslav Sumbal <slavkosster@gmail.com>
2016-07-20 16:21:46 +0200

    finished app

---
+++ src/reducers/books_reducer.js
@@ -0,0 +1,9 @@
+export default function() {
+    return [
+        {title: 'Book #1', pages: 12},
+        {title: 'Book #2', pages: 120},
+        {title: 'Book #3', pages: 22},
+        {title: 'Book #4', pages: 312},
+        {title: 'Book #5', pages: 1212}
+    ]
+}

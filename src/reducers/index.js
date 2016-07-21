3721a7367da8910abd9adf7a493653b253d98f1d
Jaroslav Sumbal <slavkosster@gmail.com>
2016-07-21 09:11:31 +0200

    Revert "finished app"
    This reverts commit 22883affbcf913bd79a7f61a17d44af7acf83bb1.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,7 @@
 import { combineReducers } from 'redux';
-import BooksReducer from './books_reducer';
-import ActiveBookReducer from './active_book_reducer';
 
 const rootReducer = combineReducers({
-  books: BooksReducer,
-  activeBook: ActiveBookReducer
+  state: (state = {}) => state
 });
 
 export default rootReducer;
